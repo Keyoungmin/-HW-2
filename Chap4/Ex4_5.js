a7a0ed8164e68ccaffe042f43de8269ed7dd067d
Keyoungmin <39690132+Keyoungmin@users.noreply.github.com>
2025-05-24 11:29:00 +0900

    Ex 4-5 update
    Array.prototype.map 구현

---
+++ Chap4/Ex4_5.js
@@ -0,0 +1,9 @@
+// 예제 4-5 콜백 함수 예제 (2-3) - Array.prototype.map - 구현
+Array.prototype.map = function (callback, thisArg) {
+    var mappedArr = [];
+    for (var i = 0; i < this.length; i++) {
+      var mappedValue = callback.call(thisArg || window, this[i], i, this);
+      mappedArr.push(mappedValue);
+    }
+    return mappedArr;
+  };
