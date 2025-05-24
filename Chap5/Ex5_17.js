1d05ccf635da5288211447a381adc00679156065
Keyoungmin <39690132+Keyoungmin@users.noreply.github.com>
2025-05-24 15:03:17 +0900

    Ex 5-17 update
    커링 함수(1)

---
+++ Chap5/Ex5_17.js
@@ -0,0 +1,12 @@
+// 예제 5-17 커링 함수(1)
+var curry5 = function (func) {
+    return function (a) {
+      return function (b) {
+        return func(a, b);
+      };
+    };
+  };
+  
+  var getMaxWith10 = curry5(Math.max)(10);
+  console.log(getMaxWith10(5));    // 10
+  console.log(getMaxWith10(25));   // 25
