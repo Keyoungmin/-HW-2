c2e1651c7fb9fde561971d790480f3f44e813f96
Keyoungmin <39690132+Keyoungmin@users.noreply.github.com>
2025-05-24 13:19:24 +0900

    Ex 5-9 update
    콜백 함수와 클로저(4)

---
+++ Chap5/Ex5_9.js
@@ -0,0 +1,17 @@
+// 예제 5-9 콜백 함수와 클로저(4)
+var fruits = ['apple', 'banana', 'peach'];
+var $ul = document.createElement('ul');
+
+var alertFruitBuilder = function (fruit) {
+    return function () {
+      alert('your choice is - ' + fruit);
+    };
+  };
+
+  fruits.forEach(function (fruit) {
+    var $li = document.createElement('li');
+    $li.innerText = fruit;
+    $li.addEventListener('click', alertFruitBuilder(fruit));
+    $ul.appendChild($li);
+  });
+  document.body.appendChild($ul);
