a533b3c66900ae7f7108bdd5bda9349ecb7651a0
Keyoungmin <39690132+Keyoungmin@users.noreply.github.com>
2025-05-24 12:30:47 +0900

    Ex 5-4 update
    return 없이도 클로저가 발생하는 다양한 경우

---
+++ Chap5/Ex5_4_(1).js
@@ -0,0 +1,15 @@
+// 예제 5-4 return 없이도 클로저가 발생하는 다양한 경우
+// (1) setInterval/setTimeout
+(function () {
+    var a = 0;
+    var intervalId = null;
+    var inner = function () {
+        if (++a >= 10) {
+            clearInterval(intervalId);
+        }
+        console.log(a);
+    };
+    intervalId = setInterval(inner, 1000);
+})();
+
+
